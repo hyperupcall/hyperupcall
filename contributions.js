e889796ac17a6e5f7ce4b8aebb7d3af718fee6f8
Edwin Kofler <edwin@kofler.dev>
2024-01-13 00:45:47 -0800

    More fixes and tweaks

--- contributions.js
+++ contributions.js
@@ -77,119 +77,118 @@ function isExcludedRepository(repo) {
 	}
 }
 
-// {
-// 	// Get all pull requests for owner
-// 	const pullRequests =
-// 		JSON.parse(await fs.readFile(PULL_REQUESTS_FILE, 'utf-8')).pullRequests ?? []
-// 	const datesJson = JSON.parse(await fs.readFile(DATES_FILE, 'utf-8'))
-// 	let createdAt = ''
-// 	if (datesJson?.pull_requests) {
-// 		createdAt = `created:>${datesJson.pull_requests}`
-// 	}
+{
+	// Get all pull requests for owner
+	const Prs =
+		JSON.parse(await fs.readFile(PULL_REQUESTS_FILE, 'utf-8')).pullRequests ?? []
+	const datesJson = JSON.parse(await fs.readFile(DATES_FILE, 'utf-8'))
+	let createdAt = ''
+	if (datesJson?.pull_requests) {
+		createdAt = `created:>${datesJson.pull_requests}`
+	}
 
-// 	const iterator = octokit.paginate.iterator(octokit.rest.search.issuesAndPullRequests, {
-// 		q: `is:pr author:${config.owner} ${createdAt}`,
-// 		sort: 'created',
-// 		order: 'asc',
-// 	})
-// 	for await (const { data: prs } of iterator) {
-// 		for (const pr of prs) {
-// 			const { owner, repoName } = getURLData(pr.html_url)
-// 			const createdAt = new Date(pr.created_at)
-// 			const millisecondsInADay = 24 * 60 * 60 * 1000
-// 			const lastSearched = new Date(createdAt - millisecondsInADay)
+	const iterator = octokit.paginate.iterator(octokit.rest.search.issuesAndPullRequests, {
+		q: `is:pr author:${config.owner} ${createdAt}`,
+		sort: 'created',
+		order: 'asc',
+	})
+	for await (const { data: prs } of iterator) {
+		for (const pr of prs) {
+			const { owner, repoName } = getURLData(pr.html_url)
+			const createdAt = new Date(pr.created_at)
+			const millisecondsInADay = 24 * 60 * 60 * 1000
+			const lastSearched = new Date(createdAt - millisecondsInADay)
 
-// 			const link = ansiEscapes.link(
-// 				`${owner}/${repoName}/pulls/${pr.number}`,
-// 				pr.html_url,
-// 			)
-// 			console.log(`Processing ${createdAt.toISOString()} (${link})`)
-// 			if (
-// 				!pullRequests.some((item) => {
-// 					return item.node_id === pr.node_id
-// 				}) &&
-// 				pr.pull_request.merged_at
-// 			) {
-// 				pullRequests.push(pr)
-// 			}
-// 			await updateFile(PULL_REQUESTS_FILE, {
-// 				pullRequests,
-// 			})
-// 			await updateFile(DATES_FILE, {
-// 				pull_requests: lastSearched.toISOString(),
-// 			})
-// 		}
-// 	}
-// }
+			const link = ansiEscapes.link(
+				`${owner}/${repoName}/pulls/${pr.number}`,
+				pr.html_url,
+			)
+			console.log(`Processing ${createdAt.toISOString()} (${link})`)
+			if (
+				!Prs.some((item) => {
+					return item.node_id === pr.node_id
+				})
+			) {
+				Prs.push(pr)
+			}
+			await updateFile(PULL_REQUESTS_FILE, {
+				pullRequests: Prs,
+			})
+			await updateFile(DATES_FILE, {
+				pull_requests: lastSearched.toISOString(),
+			})
+		}
+	}
+}
 
-// {
-// 	// Create a list of all repositories contributed to
-// 	const repositories = []
-// 	const prs =
-// 		JSON.parse(await fs.readFile(PULL_REQUESTS_FILE, 'utf-8')).pullRequests ?? []
-// 	for (const pr of prs) {
-// 		const { owner, repoName } = getURLData(pr.html_url)
-// 		const repo = `${owner}/${repoName}`
-// 		if (!repositories.includes(repo) && !isExcludedRepository(repo)) {
-// 			repositories.push(repo)
-// 		}
-// 	}
-// 	await updateFile(REPOSITORIES_FILE, {
-// 		repositories,
-// 	})
-// }
+{
+	// Create a list of all repositories contributed to
+	const repositories = []
+	const prs =
+		JSON.parse(await fs.readFile(PULL_REQUESTS_FILE, 'utf-8')).pullRequests ?? []
+	for (const pr of prs) {
+		const { owner, repoName } = getURLData(pr.html_url)
+		const repo = `${owner}/${repoName}`
+		if (!repositories.includes(repo) && !isExcludedRepository(repo)) {
+			repositories.push(repo)
+		}
+	}
+	await updateFile(REPOSITORIES_FILE, {
+		repositories,
+	})
+}
 
-// {
-// 	// Get all commits  for given repositories
-// 	const repositories =
-// 		JSON.parse(await fs.readFile(REPOSITORIES_FILE, 'utf-8')).repositories ?? []
-// 	for (const repository of repositories) {
-// 		const commits =
-// 			JSON.parse(await fs.readFile(COMMITS_FILE, 'utf-8'))?.commits?.[repository] ?? []
-// 		const datesJson = JSON.parse(await fs.readFile(DATES_FILE, 'utf-8'))
-// 		let createdAt = ''
-// 		if (datesJson?.commits?.[repository]) {
-// 			createdAt = `created:>${datesJson.commits[repository]}`
-// 		}
+{
+	// Get all commits  for given repositories
+	const repositories =
+		JSON.parse(await fs.readFile(REPOSITORIES_FILE, 'utf-8')).repositories ?? []
+	for (const repository of repositories) {
+		const Commits =
+			JSON.parse(await fs.readFile(COMMITS_FILE, 'utf-8'))?.commits?.[repository] ?? []
+		const datesJson = JSON.parse(await fs.readFile(DATES_FILE, 'utf-8'))
+		let createdAt = ''
+		if (datesJson?.commits?.[repository]) {
+			createdAt = `created:>${datesJson.commits[repository]}`
+		}
 
-// 		const iterator = octokit.paginate.iterator(octokit.rest.search.commits, {
-// 			q: `author:${config.owner} repo:${repository} ${createdAt}`,
-// 			sort: 'created',
-// 			order: 'asc',
-// 		})
-// 		for await (const { data: commits } of iterator) {
-// 			for (const commit of commits) {
-// 				const { owner, repoName } = getURLData(commit.html_url)
-// 				const createdAt = new Date(commit.commit.committer.date)
-// 				const millisecondsInADay = 24 * 60 * 60 * 1000
-// 				const lastSearched = new Date(createdAt - millisecondsInADay)
-// 				const link = ansiEscapes.link(
-// 					`${owner}/${repoName}/${commit.commit.tree.sha}`,
-// 					commit.html_url,
-// 				)
-// 				console.log(`Processing ${createdAt.toISOString()} (${link})`)
+		const iterator = octokit.paginate.iterator(octokit.rest.search.commits, {
+			q: `author:${config.owner} repo:${repository} ${createdAt}`,
+			sort: 'created',
+			order: 'asc',
+		})
+		for await (const { data: commits } of iterator) {
+			for (const commit of commits) {
+				const { owner, repoName } = getURLData(commit.html_url)
+				const createdAt = new Date(commit.commit.committer.date)
+				const millisecondsInADay = 24 * 60 * 60 * 1000
+				const lastSearched = new Date(createdAt - millisecondsInADay)
+				const link = ansiEscapes.link(
+					`${owner}/${repoName}/${commit.commit.tree.sha}`,
+					commit.html_url,
+				)
+				console.log(`Processing ${createdAt.toISOString()} (${link})`)
 
-// 				if (
-// 					!commits.some((item) => {
-// 						return item.node_id === commit.node_id
-// 					})
-// 				) {
-// 					commits.push(commit)
-// 				}
-// 				await updateFile(COMMITS_FILE, {
-// 					commits: {
-// 						[repository]: commits,
-// 					},
-// 				})
-// 				await updateFile(DATES_FILE, {
-// 					commits: {
-// 						[repository]: lastSearched.toISOString(),
-// 					},
-// 				})
-// 			}
-// 		}
-// 	}
-// }
+				if (
+					!Commits.some((item) => {
+						return item.node_id === commit.node_id
+					})
+				) {
+					Commits.push(commit)
+				}
+				await updateFile(COMMITS_FILE, {
+					commits: {
+						[repository]: Commits,
+					},
+				})
+				await updateFile(DATES_FILE, {
+					commits: {
+						[repository]: lastSearched.toISOString(),
+					},
+				})
+			}
+		}
+	}
+}
 
 {
 	// Write 'contributions.md
@@ -204,16 +203,23 @@ function isExcludedRepository(repo) {
 		const totalPrs =
 			prs.filter((item) => {
 				const itemData = getURLData(item.html_url)
-				return itemData.owner === owner && itemData.repoName === repoName
-			})?.length ?? '?'
-		const totalCommits =
-			JSON.parse(await fs.readFile(COMMITS_FILE, 'utf-8')).commits?.[repo]?.length ?? '?'
 
-		repositoryInfo.push({
-			repo,
-			totalPrs,
-			totalCommits,
-		})
+				return (
+					itemData.owner === owner &&
+					itemData.repoName === repoName &&
+					item.pull_request.merged_at
+				)
+			})?.length ?? null
+		const totalCommits =
+			JSON.parse(await fs.readFile(COMMITS_FILE, 'utf-8')).commits?.[repo]?.length ?? null
+
+		if (totalPrs > 0 || totalCommits > 0) {
+			repositoryInfo.push({
+				repo,
+				totalPrs,
+				totalCommits,
+			})
+		}
 	}
 
 	repositoryInfo.sort((a, b) => {
@@ -223,11 +229,25 @@ function isExcludedRepository(repo) {
 	let readmeStr = `# Contributions\n
 This list is automatically generated. It is ordered by number of pull requests.\n\n`
 	for (const { repo, totalPrs, totalCommits } of repositoryInfo) {
-		let commitPart = ''
-		if (totalCommits !== '?') {
-			commitPart = `, ${totalCommits} [commits](https://github.com/${repo}/commits?author=${config.owner})`
+		let prPart = ''
+		if (totalPrs) {
+			prPart = `${totalPrs} [${
+				totalPrs === 1 ? 'pull request' : 'pull requests'
+			}](https://github.com/${repo}/pulls?q=author%3A${
+				config.owner
+			}+is%3Apr+is%3Amerged+sort%3Aupdated-desc)`
 		}
-		readmeStr += `- [${repo}](https://github.com/${repo}) (${totalPrs} [pull requests](https://github.com/${repo}/pulls?q=author%3A${config.owner}+is%3Apr+is%3Amerged+sort%3Aupdated-desc)${commitPart})\n`
+
+		let commitPart = ''
+		if (totalCommits) {
+			if (totalPrs > 0) {
+				commitPart += ', '
+			}
+			commitPart += `${totalCommits} [${
+				totalCommits === 1 ? 'commit' : 'commits'
+			}](https://github.com/${repo}/commits?author=${config.owner})`
+		}
+		readmeStr += `- [${repo}](https://github.com/${repo}) (${prPart}${commitPart})\n`
 	}
 
 	await fs.writeFile(CONTRIBUTIONS_FILE, readmeStr)
